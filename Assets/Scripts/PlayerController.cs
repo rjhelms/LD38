1733339b3122ce89e0ea07ceb65e4ac6a1e78b0d
Rob Hailman <robhailman@gmail.com>
2017-04-22 17:40:33 -0400

    Character controller work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
 
     public float MoveForce = 1f;
     public float JumpForce = 1f;
+    public float PlayerRadius = 16f;
+    public LayerMask GroundLayerMask;
+
     // Use this for initialization
     void Start()
     {
@@ -21,11 +24,22 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        GetComponent<Rigidbody2D>().AddForce(new Vector2(Input.GetAxis("Horizontal") * MoveForce, 0));
-        if (Input.GetButtonDown("Fire1"))
+        Collider2D grounded = Physics2D.OverlapCircle(transform.position, PlayerRadius, GroundLayerMask);
+
+        if (grounded)
         {
-            Debug.Log("Jumping!");
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(Input.GetAxis("Horizontal") * MoveForce, 0));
+            if (Input.GetButton("Fire1"))
+            {
+                Debug.Log("Jumping!");
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            }
+        } else
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                Debug.Log("Can't jump");
+            }
         }
     }
 }
