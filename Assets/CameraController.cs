8bc2d7c0c587de7dfd3b4fafc66a23980dc8d120
Rob Hailman <robhailman@gmail.com>
2017-04-22 18:21:03 -0400

    Add camera following behaviour

---
+++ Assets/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Rigidbody2D Player;
+    public float ForecastMultiplier;
+    public float LerpFactor = 0.2f;
+    public float TargetY = 96;
+    public float TargetZ = -10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float x_position = Player.transform.position.x;
+        x_position += Player.velocity.x * ForecastMultiplier;
+        x_position = Mathf.Lerp(transform.position.x, x_position, LerpFactor);
+        transform.position = new Vector3(x_position, TargetY, TargetZ);
+	}
+}
