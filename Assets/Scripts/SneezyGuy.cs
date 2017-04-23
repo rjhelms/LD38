6cbea632f5a79f0db914eb7d5a5e4a5cbbf06115
Rob Hailman <robhailman@gmail.com>
2017-04-23 14:12:53 -0400

    Add sneezy guy
    Doesn't move, just sneezes

---
+++ Assets/Scripts/SneezyGuy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SneezyGuy : MonoBehaviour {
+
+    public ParticleSystem SneezeEmitter;
+    public float SneezeCheckTime;
+    public float SneezeChance;
+
+    private GameController gameController;
+    private float nextSneezeCheckTime;
+	// Use this for initialization
+	void Start () {
+        gameController = FindObjectOfType<GameController>();
+        nextSneezeCheckTime = Time.fixedTime + SneezeCheckTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if (gameController.State == GameController.GameState.RUNNING)
+        {
+            if (Time.fixedTime > nextSneezeCheckTime)
+            {
+                nextSneezeCheckTime = Time.fixedTime + SneezeCheckTime;
+                float willSneeze = Random.Range(0f, 1f);
+                if (willSneeze <= SneezeChance)
+                {
+                    Debug.Log(willSneeze);
+                    Debug.Log("Achoo!");
+                    SneezeEmitter.Play();
+                }
+            }
+        }
+    }
+}
